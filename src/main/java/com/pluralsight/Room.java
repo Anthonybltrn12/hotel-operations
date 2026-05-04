a81171d4fd2571f83a7b6653ed9cceb258a6a4c9
antbeltran <anthonybltrn@gmail.com>
2026-05-04 14:44:36 -0500

    room class is set up

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
 public class Room {
+
+    private final int numOfBeds;
+    private final double getPrice;
+    private final boolean isOccupied;
+    private final boolean isDirty;
+
+    public Room(int numOfBeds, double getPrice, boolean isOccupied, boolean isDirty) {
+        this.numOfBeds = numOfBeds;
+        this.getPrice = getPrice;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+    }
+
+    public int getNumOfBeds() {
+        return numOfBeds;
+    }
+
+    public double getGetPrice() {
+        return getPrice;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean getIsAvailable(){
+        if(isDirty == true || isOccupied == true){
+            return false;
+        }
+        return true;
+    }
 }
