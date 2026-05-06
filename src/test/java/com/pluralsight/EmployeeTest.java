daa5efbcba33e1a96739bcf9a546e77bc690b69d
antbeltran <anthonybltrn@gmail.com>
2026-05-06 13:25:45 -0500

    added tests for employee class

--- src/test/java/com/pluralsight/EmployeeTest.java
+++ src/test/java/com/pluralsight/EmployeeTest.java
@@ -1,7 +1,62 @@
 package com.pluralsight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+
+    @Test
+    public void hours_should_be_totaled_correctly_after_shift(){
+        //arrange
+        Employee ant = new Employee(12,"Ant","Software",35.00, 0);
+        ant.punchIn(7);
+        ant.punchOut(12);
+        int expectedOutput = 5;
+
+        //act
+        int actualOutput = ant.getHoursWorked();
+
+        //assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void total_pay_should_be_added(){
+        //arrange
+        Employee ant = new Employee(12,"Ant","Software",10.00, 20);
+        double expectedOutput = 200;
+
+        //act
+        double actualOutput = ant.getTotalPay();
+
+        //assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+    @Test
+    public void pay_should_reflect_overtime_hours(){
+        //arrange
+        Employee ant = new Employee(12,"Ant","Software",10.00, 50);
+        double expectedOutput = 550;
+
+        //act
+        double actualOutput = ant.getTotalPay();
+
+        //assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void should_get_correct_amount_of_overTime_hours(){
+        //arrange
+        Employee ant = new Employee(12,"Ant","Software",10.00, 60);
+        int expectedOutput = 20;
+
+        //act
+        int actualOutput = ant.getOverTimeHours();
+
+        //assert
+        assertEquals(expectedOutput, actualOutput);
+    }
 
 }
